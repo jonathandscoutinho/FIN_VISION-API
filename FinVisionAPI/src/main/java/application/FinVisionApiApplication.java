34344c73d438167c457ef8c8c5193783a5b4247f
Jonathan Coutinho <jonathandscoutinho@gmail.com>
2024-05-17 20:07:03 -0300

    project schema update

---
+++ FinVisionAPI/src/main/java/application/FinVisionApiApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinVisionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinVisionApiApplication.class, args);
+	}
+
+}
---
+++ FinVisionAPI/src/test/java/com/example/FinVisionAPI/FinVisionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinVisionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinVisionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
